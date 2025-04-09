2a80712698c194abd587f4462b4969dbb9bece22
amine <aminebichiou2@gmail.com>
2025-04-09 16:54:48 +0100

    icons add

--- PI/src/main/java/com/example/auth/controller/DashboardController.java
+++ PI/src/main/java/com/example/auth/controller/DashboardController.java
@@ -6,14 +6,48 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DashboardController {
+
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private VBox mainContent;
+    @FXML
+    private Hyperlink dashboardButton;
+    @FXML
+    private Hyperlink achat;
+    @FXML
+    private Hyperlink productButton;
+    @FXML
+    private Hyperlink categoryButton;
+    @FXML
+    private Hyperlink userButton;
+    @FXML
+    private Hyperlink settings;
+    @FXML
+    private Hyperlink logoutButton;
+
     @FXML private Label welcomeLabel;
     @FXML private Label emailLabel;
+
+
+    private void loadContent(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            VBox content = loader.load();
+            borderPane.setCenter(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private SessionManager sessionManager = SessionManager.getInstance();
 
@@ -27,6 +61,20 @@ public class DashboardController {
 
         welcomeLabel.setText("Welcome, " + user.getPrenom() + " " + user.getNom() + "!");
         emailLabel.setText("Email: " + user.getEmail());
+
+        dashboardButton.setOnAction(event -> loadContent("/com/example/pages/dashboard.fxml"));
+        achat.setOnAction(event -> loadContent("/com/example/pages/purchasing.fxml"));
+        productButton.setOnAction(event -> loadContent("/com/example/pages/products.fxml"));
+        categoryButton.setOnAction(event -> loadContent("/com/example/pages/categories.fxml"));
+        userButton.setOnAction(event -> loadContent("/com/example/auth/adminDashboard.fxml"));
+        settings.setOnAction(event -> loadContent("/com/example/pages/settings.fxml"));
+        logoutButton.setOnAction(event -> {
+            try {
+                handleLogout();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     @FXML
