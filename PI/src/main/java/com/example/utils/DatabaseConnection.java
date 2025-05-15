6b62519451ea20ad27103e3048f8524aabe9a6f3
amine <aminebichiou2@gmail.com>
2025-05-15 04:35:05 +0100

    produit sans image symfony  event rec user

--- PI/src/main/java/com/example/utils/DatabaseConnection.java
+++ PI/src/main/java/com/example/utils/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
     private static DatabaseConnection instance;
     private Connection connection;
 
-    private static final String URL = "jdbc:mysql://localhost:3306/pidevv";
+    private static final String URL = "jdbc:mysql://localhost:3306/pides";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
